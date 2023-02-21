2317aff70e285a42941c47a4205081e46b9778a6
Orekhov-Illia <ilorehov97@gmail.com>
2023-02-21 17:40:15 +0200

    HM 15 ready

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,11 @@ $(document).ready(function(){
         }
     });
 });
+
+
+var burgerMenu = document.getElementById('burger-menu');
+var overlay = document.getElementById('menu');
+burgerMenu.addEventListener('click',function(){
+    this.classList.toggle("close");
+    overlay.classList.toggle("overlay");
+});
